e90ecd5293cd0a16df80cab7cd13579ec49b84e7
mingmoth <mingmoth@gmail.com>
2022-04-04 22:34:36 +0800

    modify: browse to hash route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getCurrentUser } from './store/actions/userAction'
 
